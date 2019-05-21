282e31da278132cdd1930cb603f1be331eae11fa
Amar <1211amarsingh@gmail.com>
2019-05-21 14:25:06 +0530

    demo update

--- app/src/main/java/com/kv/imageviewpagerwithimagezoom/MainActivity.java
+++ app/src/main/java/com/kv/imageviewpagerwithimagezoom/MainActivity.java
@@ -4,7 +4,9 @@ import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.kv.popupimageview.PopupImageView;
 
 import java.util.ArrayList;
@@ -13,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
     private Context context;
     private ArrayList<String> arr_img = new ArrayList<>();
+    ImageView imageView1, imageView2, imageView3, imageView4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,26 +29,46 @@ public class MainActivity extends AppCompatActivity {
         arr_img.add("https://loremflickr.com/640/360");
         arr_img.add("https://placekitten.com/640/360");
         arr_img.add("https://www.fillmurray.com/640/360");
-        arr_img.add("http://via.placeholder.com/640x360");
-        arr_img.add("https://dummyimage.com/640x360/fff/aaa");
+        arr_img.add("https://picsum.photos/id/237/200/300");
+        arr_img.add("https://picsum.photos/200/300/?blur");
+        arr_img.add("https://picsum.photos/200/300?grayscale");
+
+        imageView1 = findViewById(R.id.img_1);
+        imageView2 = findViewById(R.id.img_2);
+        imageView3 = findViewById(R.id.img_3);
+        imageView4 = findViewById(R.id.img_4);
+
+        Glide.with(context).load(arr_img.get(0)).into(imageView1);
+        Glide.with(context).load(arr_img.get(1)).into(imageView2);
+        Glide.with(context).load(arr_img.get(2)).into(imageView3);
+        Glide.with(context).load("https://picsum.photos/id/12/2500/1667").into(imageView4);
     }
 
-
     public void imgclick1(View view) {
-        new PopupImageView(context, view, arr_img, 0);
+        new PopupImageView(context, view, "https://loremflickr.com/640/360");
     }
 
     public void imgclick2(View view) {
-        new PopupImageView(context, view, arr_img, 1);
-
-    }
-
-    public void imgclick3(View view) {
         new PopupImageView(context, view, arr_img);
 
     }
 
+    public void imgclick3(View view) {
+        new PopupImageView(context, view, arr_img,2);
+    }
+
+
     public void imgclick4(View view) {
-        new PopupImageView(context, view, "https://loremflickr.com/640/360");
+        String base_url = "https://picsum.photos/";
+        ArrayList<String> arrayListImage = new ArrayList<>();
+        arrayListImage.add("200/300?random=1");
+        arrayListImage.add("200/300?random=2");
+        arrayListImage.add("200/300?random=3");
+        arrayListImage.add("200/300?random=4");
+        arrayListImage.add("200/300?random=5");
+        arrayListImage.add("200/300?random=6");
+        arrayListImage.add("200/300?random=7");
+
+        new PopupImageView(context, view, arrayListImage, 1, base_url);
     }
 }
